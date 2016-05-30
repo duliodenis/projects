62066a26533215b21eb1c71111b01dc9b03fd89b
Dulio Denis <dulio.denis@yahoo.com>
2016-05-30 14:33:38 -0400

    Added Custom TableViewCell for MainTableView

---
+++ Projects/Projects/Controllers/MainTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableViewCell.swift
+//  Projects
+//
+//  Created by Dulio Denis on 5/30/16.
+//  Copyright © 2016 Dulio Denis. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
